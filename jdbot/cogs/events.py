6a73fc9f8aa7beba2992bfc21747afc739a6a704
JD <jellisduncan@gmail.com>
2020-12-21 04:50:04 -0600

    add uptime command

--- jdbot/cogs/events.py
+++ jdbot/cogs/events.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from datetime import datetime
 
 class Events(commands.Cog):
     def __init__(self, client):
@@ -8,6 +9,7 @@ class Events(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'{self.client.user} is online.')
+        self.client.launch_time = datetime.utcnow()
 
 def setup(client):
     client.add_cog(Events(client))
--- jdbot/cogs/util_commands.py
+++ jdbot/cogs/util_commands.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from datetime import datetime
 
 class Utils(commands.Cog):
     def __init__(self, client):
@@ -8,6 +9,14 @@ class Utils(commands.Cog):
     @commands.command()
     async def ping(self, ctx):
         await ctx.send(f'Pong!\nLatency is {self.client.latency}')
+
+    @commands.command()
+    async def uptime(self, ctx):
+        delta_uptime = datetime.utcnow() - self.client.launch_time
+        hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
+        minutes, seconds = divmod(remainder, 60)
+        days, hours = divmod(hours, 24)
+        await ctx.send(f"{days}d, {hours}h, {minutes}m")
 
 def setup(client):
     client.add_cog(Utils(client))
